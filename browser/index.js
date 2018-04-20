41c01bf1c8fccfa75f8047857fdfd5d71830f993
Eric RENARD <timmy469@gmail.com>
2018-04-20 14:07:15 +0200

    eslint

--- browser/index.js
+++ browser/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable */
-import MessageLogger from './message-logger'
-import ArrayAverager from './array-averager'
+// import MessageLogger from './message-logger'
+// import ArrayAverager from './array-averager'
+import Cluster from './cluster'
